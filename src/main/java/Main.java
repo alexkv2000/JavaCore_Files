1015a8a33b4fcc81109c42037d8859bb62f25a0a
AlexKv2000 <AlexKv2000@mail.ru>
2022-06-21 10:54:09 +0300

    edit structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,22 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
+
+        String path = "D:";
         List<File> folderList = Arrays.asList(
-                new File("D://Games"),
-                new File("D://Games//temp"),
-                new File("D://Games//src"),
-                new File("D://Games//res"),
-                new File("D://Games//savegames"),
-                new File("D://Games//src//main"),
-                new File("D://Games//src//test"),
-                new File("D://Games//res//drawables"),
-                new File("D://Games//res//vectors"),
-                new File("D://Games//res//icons")
+                new File(path),
+                new File(path + "//Games"),
+                new File(path + "//Games//temp"),
+                new File(path + "//Games//src"),
+                new File(path + "//Games//res"),
+                new File(path + "//Games//savegames"),
+                new File(path + "//Games//src//main"),
+                new File(path + "//Games//src//test"),
+                new File(path + "//Games//res//drawables"),
+                new File(path + "//Games//res//vectors"),
+                new File(path + "//Games//res//icons")
         );
         List<File> fileList = Arrays.asList(
-                new File("D://Games//src//main//Main.java"),
-                new File("D://Games//src//main//Utils.java"),
-                new File("D://Games//temp//temp.txt")
+                new File(path + "//Games//src//main//Main.java"),
+                new File(path + "//Games//src//main//Utils.java"),
+                new File(path + "//Games//temp//temp.txt")
         );
         folderList.stream().forEach(folder -> {
             if (folder.mkdir()) sb.append("Каталог " + folder + " создан\n");
@@ -35,13 +38,13 @@ public class Main {
                 sb.append(ex.getMessage() + '\n');
             }
         });
-        try (FileWriter log = new FileWriter("D://Games//temp//temp.txt", false)) {
+        try (FileWriter log = new FileWriter(path + "//Games//temp//temp.txt", false)) {
             log.write(sb.toString());
             log.flush();
         } catch (IOException ex) {
             sb.append(ex.getMessage() + '\n');
         }
-        try (BufferedReader br = new BufferedReader(new FileReader("D://Games//temp//temp.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path + "//Games//temp//temp.txt"))) {
             String s;
             while ((s = br.readLine()) != null) System.out.println(s);
         } catch (IOException ex) {
